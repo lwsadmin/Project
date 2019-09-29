1336effb367286aeb78f263244059c288c0b56f8
lwsadmin <39931938+lwsadmin@users.noreply.github.com>
2019-09-29 15:46:45 +0800

    2019-09-29
    delete  2.2

--- Project.MobileWeb/Controllers/HomeController.cs
+++ Project.MobileWeb/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Project.Application.IAppService;
 using Project.MobileWeb.Models;
 
 namespace Project.MobileWeb.Controllers
@@ -12,17 +13,35 @@ namespace Project.MobileWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUserAppService _userAppService;
+        public HomeController(ILogger<HomeController> logger, IUserAppService userAppService)
         {
             _logger = logger;
+            _userAppService = userAppService;
         }
 
         public IActionResult Index()
         {
+            _logger.LogError(".NET Core3.0自带了日志---------------");
+            ViewBag.User = _userAppService.GetByIdAsync(3).Result;
+            ViewBag.List = _userAppService.GetFields(c => c.Name, c => c.Id > 0);
+            var userList = _userAppService.GetFields(c => new { c.Id, c.UserName }, c => c.Id > 0);
+            foreach (var item in userList)
+            {
+                var s = item.Id;
+            }
+            ViewBag.UserList = userList.ToList().Select(s => Tuple.Create(s.Id, s.UserName));
             return View();
         }
-
+        private T GetData<T>(List<T> list, Func<T, bool> predicate)
+        {
+            var s = list.Where(predicate).FirstOrDefault();
+            if (s == null)
+            {
+                s = System.Activator.CreateInstance<T>();
+            }
+            return s;
+        }
         public IActionResult Privacy()
         {
             return View();
--- Project.MobileWeb/Startup.cs
+++ Project.MobileWeb/Startup.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,13 +24,16 @@ namespace Project.MobileWeb
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // 此方法由运行时调用。使用此方法将服务添加到容器。
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //services.AddDbContext<EFContext>(options => options.UseSqlServer("Server=.;Database=Project;User=sa;Password=123456;"));
+            //设置依赖注入
+            SetDepend("Project.Application", services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // 此方法由运行时调用。使用此方法配置http请求管道。
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,23 +42,38 @@ namespace Project.MobileWeb
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler(c =>
+                {
+                    c.Run(async d =>
+                    {
+                        var ex = d.Features.Get<IExceptionHandlerFeature>();
+                        if (ex != null)
+                        {
+                            //记录异常日志
+                            var path = $"d:\\{DateTime.Now.ToString("yyyy-MM-dd")}.txt";
+                            System.IO.StreamWriter sw = new System.IO.StreamWriter(path, true);
+                            sw.WriteLine(ex.Error.Message);
+                            sw.WriteLine(ex.Error.StackTrace);
+                            sw.Flush();
+                            sw.Close();
+                        }
+                        await d.Response.WriteAsync(ex?.Error?.Message ?? "an error occure");
+                    });
+                });
+              //  app.UseExceptionHandler("/Home/Error");
+                // 默认hsts值为30天。您可能需要为生产场景更改此设置，请参阅https://aka.ms/aspnetcore-hsts。
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
+            app.UseHttpsRedirection();//http强制跳转https
+            app.UseStaticFiles();//静态资源
+            app.UseRouting();//注入路由
+            app.UseAuthorization();//身份认证，必须在UseEndpoints之前
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            });//注入端点 默认路由表
         }
 
         private void SetDepend(string assemblyName, IServiceCollection services)
